c46a0be8471c8764c0064db2a082e1d4c1267217
Ahsan Akhtar <ahsanakhtar91@yahoo.com>
2025-04-23 01:47:23 +0500

    Added `react-leaflet` package, rendered the initial map centered at a place with marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-function App() {
-  return <div>Hello World</div>;
+export default function App() {
+  return (
+    <div>
+      <div className="top">
+        <div className="heading">Store Locator</div>
+      </div>
+      <MapContainer center={[33.7141061, 73.0657246]} zoom={15}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[33.7141061, 73.0657246]}>
+          <Popup>Blue Area, Islamabad</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
-
-export default App;
